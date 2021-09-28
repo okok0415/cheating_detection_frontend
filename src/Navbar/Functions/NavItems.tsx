563969e6caab955e905de8636913a1d0438da729
okok0415 <dlawjdals11@naver.com>
2021-09-28 16:21:10 +0900

    210928 Navbar Finish

--- src/Navbar/Functions/NavItems.tsx
+++ src/Navbar/Functions/NavItems.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import "../CSS/Navbar.css";
 
 function NavItem(props: any) {
     const [open, setOpen] = useState(false);
 
+    const replaceURL = () => {
+        window.location.replace(props.url);
+    }
+
     return (
-        <li className="nav-item">
-            <Link to={props.url} className="icon-button" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
+        <li className={props.border ? 'nav-item line-test' : 'nav-item'}>
+            <Link to={props.url} className='icon-button' onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} onClick={replaceURL}>
                 {props.item}
             </Link>
 
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavItem from "./Functions/NavItems";
 import { ReactComponent as CogIcon } from "./icons/cog.svg";
@@ -7,18 +7,50 @@ import { ReactComponent as CDIcon } from "./icons/cheating_detection_2.svg";
 import "./CSS/Navbar.css";
 
 function Navbar() {
+    const [test, setTest] = useState(false);
+    const [result, setResult] = useState(false);
+    const [settings, setSettings] = useState(false);
+
+    useEffect(() => {
+        if (window.location.pathname === "/") {
+            setTest(false);
+            setResult(false);
+            setSettings(false);
+        }
+        else if (window.location.pathname === "/test") {
+            setTest(true);
+            setResult(false);
+            setSettings(false);
+        } else if (window.location.pathname === "/result") {
+            setResult(true);
+            setTest(false);
+            setSettings(false);
+        } else if (window.location.pathname === "/settings") {
+            setSettings(true);
+            setTest(false);
+            setResult(false);
+        } else {
+            setTest(false);
+            setResult(false);
+            setSettings(false);
+        }
+    }, []);
+
+    const replaceURL = () => {
+        window.location.replace("/");
+    }
     return (
         <>
             <nav className="navbar">
-                <Link to='/' className="navbar-title"><CDIcon /></Link>
-                <ul className="navbar-nav">
-                    <NavItem item="Test" url="/test">
+                <Link to='/' className="navbar-title" onClick={replaceURL}><CDIcon /></Link>
+                <ul className='navbar-nav'>
+                    <NavItem item="Test" url="/test" border={test}>
 
                     </NavItem>
-                    <NavItem item="Result" url="/result">
+                    <NavItem item="Result" url="/result" border={result}>
 
                     </NavItem>
-                    <NavItem item={<CogIcon />} url="/settings">
+                    <NavItem item={<CogIcon />} url="/settings" border={settings}>
 
                     </NavItem>
                     <NavItem item={<LogoutIcon />} url="/login">

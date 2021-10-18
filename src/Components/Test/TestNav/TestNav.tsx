59e9df62c83f55477bb57de23a7cf69c6a1b4a83
okok0415 <dlawjdals11@naver.com>
2021-10-19 01:16:10 +0900

    211019 Test 6 page start!

--- src/Components/Test/TestNav/TestNav.tsx
+++ src/Components/Test/TestNav/TestNav.tsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../CSS/infNav.css";
+import { ReactComponent as ChessIcon } from "./chess-board-solid.svg";
+import { ReactComponent as CollectIcon } from "./collect.svg";
+import { ReactComponent as TestIcon } from "./laptop-house-solid.svg";
 
 function TestNav() {
+    const [calibrate, setCalibrate] = useState(false);
+    const [collect, setCollect] = useState(false);
+    const [test, setTest] = useState(false);
+
+    useEffect(() => {
+        if (window.location.pathname === "/test/calibrate") {
+            setCalibrate(true);
+            setCollect(false);
+            setTest(false);
+        }
+        else if (window.location.pathname === "/test/collect") {
+            setCalibrate(false);
+            setCollect(true);
+            setTest(false);
+        }
+        else if (window.location.pathname === "/test/test") {
+            setCalibrate(false);
+            setCollect(false);
+            setTest(true);
+        }
+        else {
+            setCalibrate(false);
+            setCollect(false);
+            setTest(false);
+        }
+    }, []);
+
+
     return (
         <>
-            <div>
-                hi
+            <div className="TestNav">
+                <div></div>
+                <div></div>
+                <div className={calibrate ? "bordernav" : ""}>
+                    <div>
+                        <div className="icontext"><ChessIcon width="40" height="40" /></div>
+                        <div className="navitem">Calibrate</div>
+                    </div>
+                </div>
+                <div></div>
+                <div className={collect ? "bordernav" : ""}>
+                    <div>
+                        <div className="icontext"><CollectIcon width="40" height="40" /></div>
+                        <div className="navitem">Collect</div>
+                    </div>
+                </div>
+                <div></div>
+                <div className={test ? "bordernav" : ""}>
+                    <div>
+                        <div className="icontext"><TestIcon width="45" height="45" /></div>
+                        <div className="navitem">Test</div>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
             </div>
         </>
     )
--- src/Components/Test/informationPage/calibrate.tsx
+++ src/Components/Test/informationPage/calibrate.tsx
@@ -1,7 +1,17 @@
 function calibrate() {
+
     return (
         <>
-            hi
+            <div className="prepare">
+                <div className="info">
+                    <div className="info-title">Calibrate 과정</div>
+                    <div className="info-content">
+                        Eye Tracking은 총 3가지 단계로 진행됩니다.<br />
+                        1.
+                    </div>
+                    <div className="btn-next">알겠습니다!</div>
+                </div>
+            </div>
         </>
     )
 }
